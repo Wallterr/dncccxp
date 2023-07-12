533da438276d034859aefc519ad2a3dd10672ceb
Wallterr <walltersteve@gmail.com>
2023-07-12 07:24:10 -0300

    criação função toggle e keydown

--- main.js
+++ main.js
@@ -8,3 +8,60 @@ function highlightCard(selector){
     var element = document.querySelector(selector);
     element.classList.toggle("card-highlight");
 }
+
+function checkKeyboardCode(){
+
+    document.addEventListener('keydown', (event) => {
+    var name = event.key;
+    var code = event.code;
+
+    alert(`Tecla pressionada ${name} \r\n Key Code: ${code}`);
+}, false); 
+}
+
+function addKeyboardEventListeners(){
+    document.addEventListener('keydown', (event) => {
+        
+        var ingresso1 = document.getElementById("quinta");
+        var ingresso2 = document.getElementById("sexta");
+        var ingresso3 = document.getElementById("sabado");
+        var ingresso4 = document.getElementById("domingo");
+
+        var code = event.code;
+        if(code == 'Digit1') {
+            ingresso1.classList.toggle("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+
+        var code = event.code;
+        if(code == 'Digit2') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.toggle("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+
+        var code = event.code;
+        if(code == 'Digit3') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.toggle("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+
+        var code = event.code;
+        if(code == 'Digit4') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.toggle("card-highlight");
+        }
+       
+    })
+}
+
+
+addKeyboardEventListeners();
+
